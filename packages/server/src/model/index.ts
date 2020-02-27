1c5b39b34a794c85a6f72e9a9bd56aa16b698005
thiagoleitedev <thiagoleitebr@gmail.com>
2020-02-27 12:26:57 -0300

    Load models

--- packages/server/src/model/index.ts
+++ packages/server/src/model/index.ts
@@ -1,1 +1,7 @@
+export { default as Account } from '../modules/account/AccountModel';
+export { default as Budget } from '../modules/budget/BudgetModel';
+export { default as Category } from '../modules/category/CategoryModel';
+export { default as CreditCard } from '../modules/credit-card/CreditCardModel';
+export { default as Tag } from '../modules/tag/TagModel';
+export { default as Transaction } from '../modules/transaction/TransactionModel';
 export { default as User } from '../modules/user/UserModel';
