5b6649392616c9f26e818a2f0188ef512c124d63
thiagoleitedev <thiagoleitebr@gmail.com>
2020-03-05 17:17:34 -0300

    Add dashboard

--- packages/web/src/components/dashboard/Dashboard.tsx
+++ packages/web/src/components/dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Content, Table } from '../ui';
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return <div>asdasd</div>;
 }
 
 export default Dashboard;
